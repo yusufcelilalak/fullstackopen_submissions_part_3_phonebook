473f37fafc0e516dab09babc1b0327eb6598076f
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-22 23:49:21 +0300

    Part3 -> changed PORT variable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 const morgan = require('morgan');
@@ -142,7 +144,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
