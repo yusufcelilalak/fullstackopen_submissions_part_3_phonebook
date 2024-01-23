85974cf1363d70640d02b0f10f78c7eecf45fced
Yusuf Celil Alak <yusuf.celil.alak@gmail.com>
2024-01-23 23:12:28 +0300

    Part3 -> Exercise 3.12 is added

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length<3) {
+    console.log('give password as argument');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://yusufcelilalak:${password}@phonebook.fgcjcju.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+if(process.argv.length === 3)
+{
+    console.log('phonebook:');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        });
+    
+        mongoose.connection.close();
+    });
+}else if(process.argv.length === 5){
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const person = new Person({
+        name: name,
+        number: number
+    });
+    
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close(url);
+    })
+}
+
+
+
